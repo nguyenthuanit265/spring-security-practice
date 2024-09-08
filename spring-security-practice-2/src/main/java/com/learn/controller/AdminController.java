70243e8fe5b26818773b74b73ff9f2cdb6c1ca1b
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-09-08 18:58:38 +0700

    spring security 2

--- spring-security-practice-2/src/main/java/com/learn/controller/AdminController.java
+++ spring-security-practice-2/src/main/java/com/learn/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.learn.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/admins")
 public class AdminController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminController.class);
 
     @GetMapping("/profiles")
     public ResponseEntity<String> getProfiles() {
+        LOGGER.info("[AdminController] getProfiles");
         return ResponseEntity.ok().body("Admin profiles");
     }
 }
